8e2ea1a41c74f5c1477f79decc0984170d9e143f
beuoon <beuoon@naver.com>
2021-02-25 20:28:47 +0900

    Add: InteractiveObject 클래스 추가
    상호작용 오브젝트의 추상 클래스인 InteractiveObject 클래스 추가

---
+++ Assets/Resources/Script/PlayScene/Tiles/InteractiveObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps; // Tile Asset 생성용
+
+public abstract class InteractiveObject : Tile
+{
+    public const string TILE_NAME = "InteractiveObject";
+    private bool IsBeenUsed;
+    protected Vector3Int Position;
+
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go) {
+        Position = position;
+        name = TILE_NAME;
+        IsBeenUsed = false;
+
+        return base.StartUp(position, tilemap, go);
+    }
+
+    public virtual bool IsAvailable() {
+        if (IsBeenUsed) return false;
+
+        int aroundPlayerCount = 0;
+        Player[] players = GameMgr.Instance.Comp_Players;
+        foreach (Player player in players) {
+            if ((player.currentTilePos - Position).magnitude < 2)
+                aroundPlayerCount++;
+        }
+
+        if (aroundPlayerCount < 1)
+            return false;
+        return true;
+    }
+
+    public virtual void Activate() {
+        if (!IsAvailable()) return;
+        IsBeenUsed = true;
+	}
+}
