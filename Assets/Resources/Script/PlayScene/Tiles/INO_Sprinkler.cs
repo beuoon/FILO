68353d07b64ded6a6bb10fead09edd5986a6c3aa
beuoon <beuoon@naver.com>
2021-02-25 20:43:09 +0900

    Add: 상호작용 오브젝트 - 스프링쿨러 추가

---
+++ Assets/Resources/Script/PlayScene/Tiles/INO_Sprinkler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class INO_Sprinkler : InteractiveObject {
+    public override void Activate() {
+        if (!IsAvailable()) return;
+        base.Activate();
+
+        List<Fire> fires = TileMgr.Instance.Fires;
+        int count = fires.Count / 2;
+
+        for (int i = 0; i < count; i++) {
+            int index = Random.Range(0, fires.Count);
+            fires.RemoveAt(index);
+		}
+    }
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/INO_Sprinkler")]
+    public static void CreateSprinkler() {
+        string path = EditorUtility.SaveFilePanelInProject("Save Sprinkler", "New Sprinkler", "asset", "Save Sprinkler", "Assets");
+        if (path == "") {
+            return;
+        }
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<INO_Sprinkler>(), path);
+    }
+#endif
+}
