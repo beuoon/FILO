a02e4cdcaf7972f9735207929db23498f43dc11f
beuoon <beuoon@naver.com>
2021-02-26 16:46:23 +0900

    Add: 상호작용 오브젝트 Door 추가

---
+++ Assets/Resources/Script/PlayScene/Tiles/INO_Door.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Tilemaps;
+using UnityEditor;
+using UnityEngine;
+using System;
+
+public class INO_Door : InteractiveObject {
+    public override bool IsAvailable() {
+        return false;
+    }
+
+    public override void Activate() {
+        base.Activate();
+
+        Debug.Log("문 작동");
+        TileMgr.Instance.SetInteractiveObject(position, null);
+        Destroy(this.gameObject);
+    }
+}
