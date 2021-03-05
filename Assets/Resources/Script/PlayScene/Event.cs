893dfd9586ba5df3948a1f973b32828c9b885441
beuoon <beuoon@naver.com>
2021-03-05 14:26:55 +0900

    Add: Simple Event 생성
    임시 Prefab, Sprite 생성
    임시 Event xml 파일 생성

---
+++ Assets/Resources/Script/PlayScene/Event.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Event {
+    public enum EventType {
+        FALLING_ROCK, SHORT_CIRCUIT, FLASHOVER, SMOKE
+    }
+
+    public readonly EventType type;
+    public readonly Vector3Int position;
+    private int leftTurn;
+
+    public Event(EventType type, Vector3Int position, int turn) {
+        this.type = type;
+        this.position = position;
+        this.leftTurn = turn;
+	}
+
+    public virtual void Update() {
+        leftTurn--;
+    }
+
+    public bool IsSatisfied {
+        get { return leftTurn <= 0; }
+    }
+
+    public static EventType StringToType(string text) {
+        switch (text) {
+        case "FALLING_ROCK": return EventType.FALLING_ROCK;
+        case "SHORT_CIRCUIT": return EventType.SHORT_CIRCUIT;
+        case "FLASHOVER": return EventType.FLASHOVER;
+        case "SMOKE": return EventType.SMOKE;
+        }
+        return EventType.FALLING_ROCK;
+    }
+}
--- Assets/Resources/Script/PlayScene/EventMgr.cs
+++ Assets/Resources/Script/PlayScene/EventMgr.cs
@@ -1,18 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
-public class EventMgr : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class EventMgr {
+    private readonly List<Event> events = new List<Event>();
+
+    public static readonly Object FallingRock   = Resources.Load("Prefabs/Event/Event_FallingRock");
+    public static readonly Object ShortCircuit  = Resources.Load("Prefabs/Event/Event_ShortCircuit");
+    public static readonly Object Flashover     = Resources.Load("Prefabs/Event/Event_Flashover");
+    public static readonly Object Smoke         = Resources.Load("Prefabs/Event/Event_Smoke");
+
+    public EventMgr(int stage) {
+        LoadEvent(stage);
+	}
+    private void LoadEvent(int stage) {
+        HashSet<int> notActivateEventIDs = new HashSet<int>();
+
+        // XML Load
+        XmlDocument doc = new XmlDocument();
+        TextAsset textAsset = (TextAsset)Resources.Load("Event/Stage" + stage);
+        doc.LoadXml(textAsset.text);
+
+        XmlNodeList eventNodes = doc.SelectNodes("Events/Event");
+        foreach (XmlNode eventNode in eventNodes) {
+            Event.EventType type = Event.StringToType(eventNode.SelectSingleNode("Type").InnerText);
+
+			string[] pointStr = eventNode.SelectSingleNode("Point").InnerText.Replace(" ", "").Split(',');
+            int x = int.Parse(pointStr[0]);
+            int y = int.Parse(pointStr[1]);
+            Vector3Int point = new Vector3Int(x, y, 0);
+
+            int turn = int.Parse(eventNode.SelectSingleNode("Turn").InnerText);
+
+            events.Add(new Event(type, point, turn));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void TurnUpdate() {
+		for (int i = 0; i < events.Count;) {
+            events[i].Update();
+
+            if (events[i].IsSatisfied) {
+                CreateEventObject(events[i]);
+                events.RemoveAt(i);
+            }
+            else
+                i++;
+		}
+	}
+
+    private void CreateEventObject(Event e) {
+        Object obj = null;
+        Vector3 pos = GameMgr.Instance.BackTile.CellToWorld(e.position);
+        pos += GameMgr.Instance.BackTile.cellSize / 2.0f;
+
+        switch (e.type) {
+        case Event.EventType.FALLING_ROCK:  obj = FallingRock;   break;
+        case Event.EventType.SHORT_CIRCUIT: obj = ShortCircuit;  break;
+        case Event.EventType.FLASHOVER:     obj = Flashover;     break;
+        case Event.EventType.SMOKE:         obj = Smoke;         break;
+        }
+
+        Object.Instantiate(obj, pos, Quaternion.identity);
     }
 }
